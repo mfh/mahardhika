2be37f001a638cbb2edabaaa6f622a6e5ff7d974
Muhammad Fuad Hasan <mfuadhasan@fastmail.fm>
2016-03-31 00:13:37 +0700

    add parser and query code

--- src/parser.rs
+++ src/parser.rs
@@ -23,4 +23,99 @@ impl Error for ParseError {
 
 pub trait ParserHandler {
     fn on_method(&mut self, _method: & str) -> Result<(), ParseError> { Ok(()) }
+    fn on_url(&mut self, _url: &str) -> Result<(), ParseError> { Ok(()) }
+    fn on_query(&mut self, _query: &str) -> Result<(), ParseError> { Ok(()) }
+    fn on_http_version(&mut self, _version: &str) -> Result<(), ParseError> { Ok(()) }
+    fn on_status(&mut self, _status: u16) -> Result<(), ParseError> { Ok(()) }
+    fn on_header(&mut self, _field: &str, _values: Vec<&str>) -> Result<(), ParseError> { Ok(()) }
+    fn on_body(&mut self, _part: &[u8]) -> Result<(), ParseError> { Ok(()) }
+    fn on_headers_complete(&mut self) -> Result<(), ParseError> { Ok(()) }
+    fn on_message_begin(&mut self) -> Result<(), ParseError> { Ok(()) }
+    fn on_message_complete(&mut self) -> Result<(), ParseError> { Ok(()) }
 }
+
+pub struct Parser<'a, H: 'a> {
+    handler: &'a mut H,
+}
+
+impl<'a, H: ParserHandler> Parser<'a, H> {
+    pub fn request(handler: &'a mut H) -> Parser<'a, H> {
+        Parser { handler: handler }
+    }
+
+    pub fn parse<R: Read>(&mut self, stream: &mut R) -> Result<(), Box<Error>> {
+        let mut buf_reader = BufReader::new(stream);
+
+        let mut request_line = String::new();
+        let bytes_read = try!(buf_reader.read_line(&mut request_line));
+
+        if bytes_read == 0 || request_line.is_empty() {
+            return Ok(());
+        }
+
+        try!(self.handler.on_message_begin());
+
+        let re = Regex::new(
+            r"^(?P<method>[A-Z]*?) (?P<url>[^\?]+)(\?(+P<query>[^#]+))? HTTP/(?P<version>\d\.\d)\r\n$"
+        ).unwrap();
+
+        match re.captures(&request_line) {
+            Some(cap) => {
+                let method = cap.name("method").unwrap();
+                try!(self.handler.on_method(method));
+
+                let url = percent_encoding::lossy_utf8_percent_decode(
+                    cap.name("url").unwrap().as_bytes()
+                );
+                try!(self.handler.on_url(&url));
+
+                match cap.name("query") {
+                    Some(query) => {
+                        let query = percent_encoding::lossy_utf8_percent_decode(query.as_bytes());
+                        try!(self.handler.on_query(&query));
+                    }
+                    None => {}
+                }
+
+                let version = cap.name("version").unwrap();
+                try!(self.handler.on_http_version(version));
+            },
+            None => {
+                let error = io::Error::new(ErrorKind::InvalidInput, "Malformed Request");
+                retur Err(Box::new(error));
+            },
+        };
+
+        // reading headers
+        for line in buf_reader.lines() {
+            match line {
+                Ok(header_line) => {
+                    // read an empty line
+                    if header_line.trim().len() == 0 {
+                        break;
+                    }
+                    let header: Vec<_> = header_line.split(": ").collect();
+
+                    if header.len() != 2 {
+                        let error = io::Error::new(
+                            ErrorKind::InvalidInput,
+                            format!("Invalid Header: '{}'", header_line),
+                        );
+                        return Err(Box::new(error));
+                    }
+
+                    let field = header[0];
+                    let values = header[1].split(',').map(|h| h.trim()).collect();
+
+                    try!(self.handler.on_header(field, values));
+                },
+                Err(e) => {
+                    return Err(Box::new(e));
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
---
+++ src/query.rs
@@ -0,0 +1,99 @@
+use std::collections::HashMap;
+
+use regex::Regex;
+
+#[derive(Debug)]
+pub struct Query {
+    data: HashMap<String, Vec<String>>,
+    query_string: Option<String>,
+}
+
+#[allow(dead_code)]
+impl Query {
+    pub fn new() -> Query {
+        Query {
+            data: HashMap::<String, Vec<string>>::new(),
+            query_string: None
+        }
+    }
+
+    pub fn from_string(query_string: &str) -> Query {
+        let mut data = HashMap::<string, Vec<String>>::new();
+
+        if query_string.trim().len() > 0 {
+            let re = Regex::new(r"([^=&]+)([^&]*))?").unwrap();
+            for cap in re.captures_iter(query_string) {
+                let key = cap.at(1).unwrap();
+                // TODO: decode query string (see this http:://unixpapa.com/js/querystring.html)
+                let val = cap.at(3)unwrap_or("");
+                let mut query-vec = data.entry(key.to_owned()).or_insert(Vec::new());
+                query_vec.push(val.to_owned());
+            }
+        }
+
+        Query {
+            data: data,
+            query_string: None,
+        }
+    }
+
+    pub fn get(&self, name: &str) -> Option<Vec<String>> {
+        match self.data.get(name) {
+            Some(values) => {
+                if values.is_empty() {
+                    None
+                } else {
+                    Some(values.clone())
+                }
+            },
+            None => None
+        }
+    }
+
+    pub fn query_string() -> Option<&str> {
+        match self.query_string {
+            Some(ref s) => Some(s),
+            None => None,
+        }
+    }
+}
+
+fn format_query_param(k: &str, v: &Vec<String>) -> String {
+    let mut resurt = String::new();
+
+    let mut k = k.to_string();
+    if v.len() > 1 {
+        k.push_str("[]");
+    }
+
+    let mut iter = v.iter();
+    match iter.next() {
+        Some(i) => result.push_str(&format!("{}={}", k, i)),
+        None => return result,
+    };
+
+    for i in iter {
+        result.push_str(&format!("&{}={}", k, i));
+    }
+    
+    result
+}
+
+impl ToString for Query {
+    fn to_string(&self) -> String {
+        let mut result = String:new();
+
+        let mut iter = self.data.iter();
+        match iter.next() {
+            Some((k, v)) => result.push_str(&format_query_param(k, v)),
+            None => return result,
+        }
+
+        for (k, v) in iter {
+            result.push_str(&format!("&{}", format_query_param(k, v)));
+        }
+
+        result
+    }
+}
+
